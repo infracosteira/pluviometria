dcd453017c9a2cea5f174502a601e24e8db82ab8
YURISAMY <isaak.yuri@aluno.unilab.edu.br>
2024-10-31 17:21:48 -0300

    Concertando os links

--- scripts python/gerador_de_links.py
+++ scripts python/gerador_de_links.py
@@ -1,26 +1,15 @@
-import csv
-import requests
+import os
 
-# URL da pasta no repositório do GitHub
-repo_url = 'https://api.github.com/repos/infracosteira/pluviometria/contents/data/postos_solo'
+caminho_pasta = 'data/postos_solo'
+url_base = 'https://github.com/infracosteira/pluviometria/raw/main/data/postos_solo/'
+links = []
 
-# Faz a requisição para obter o conteúdo da pasta
-response = requests.get(repo_url)
-if response.status_code == 200:
-    # Cria ou limpa o arquivo CSV que irá armazenar os links
-    with open('data/links_arquivos.csv', mode='w', encoding='utf-8', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Nome do Arquivo', 'Link'])  # Cabeçalho do CSV
+for nome_arquivo in os.listdir(caminho_pasta):
+    if nome_arquivo.endswith('.csv'):
+        nome_arquivo_escapado = nome_arquivo.replace(' ', '%20')
+        link = url_base + nome_arquivo_escapado
+        links.append(f'{nome_arquivo},{link}')
 
-        # Itera sobre os arquivos retornados pela API
-        for item in response.json():
-            # Verifica se o item é um arquivo CSV
-            if item['name'].endswith('.csv'):
-                nome_arquivo = item['name']
-                link_download = f"{item['download_url']}?raw=true"  # Força o download
-                
-                # Escreve o nome do arquivo e o link no CSV
-                writer.writerow([nome_arquivo, link_download])
-                print(f'Link do arquivo {nome_arquivo} adicionado ao CSV.')
-else:
-    print(f'Falha ao acessar o repositório - Código de status: {response.status_code}')
+with open('./data/links.csv', 'w') as arquivo:
+    for item in links:
+        arquivo.write(f'{item}\n')
