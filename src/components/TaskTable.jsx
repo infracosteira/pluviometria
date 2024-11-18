72901c84f1208bef89a7a5e8245b64eaef44ed5c
YURISAMY <isaak.yuri@aluno.unilab.edu.br>
2024-11-18 10:29:34 -0300

    alterando o site

--- src/components/TaskTable.jsx
+++ src/components/TaskTable.jsx
@@ -12,33 +12,40 @@ import {
 import EditableCell from "./EditableCell";
 import Filters from "./Filters";
 import SortIcon from "./icons/SortIcon";
-async function downloadFile(file) {
+import axios from 'axios';
+import JsonData from './../../data/dados_formatados_resumo.json';
+
+async function downloadFile(fileUrl) {
   try {
-
-    const response = await fetch(file, {
-      method: 'GET',
-      'mode':'no-cors',
-
+    const response = await axios.get(fileUrl, {
+      responseType: 'blob',
       headers: {
-        'Accept': "*/*",
-        'Content-Type': "*/*",
-        
-      },
+        'Content-Type': 'text/csv', 
+      },
     });
-
-    if (!response.ok) {
+    
+    console.log("response:", response);
+    
+    // Verifique se a resposta foi bem-sucedida
+    if (response.status !== 200) {
       throw new Error(`Erro ${response.status}: ${response.statusText}`);
     }
 
-    const blob = await response.blob();
+    const blob = response.data; // response.data já é um Blob
+    console.log("blob:", blob);
+    
     const link = document.createElement('a');
     link.href = window.URL.createObjectURL(blob);
-    link.download = "file.csv";
+    link.download = fileUrl.split('/').pop(); // Define o nome do arquivo a partir da URL
     link.click();
+
+    // Limpa o objeto URL após o uso
+    window.URL.revokeObjectURL(link.href);
   } catch (error) {
-    console.error('Error fetching or processing CSV:', error);
+    console.error('Erro ao buscar ou processar o arquivo CSV:', error);
   }
-};
+}
+
 const columns = [
   {
     //coluna "id"
@@ -60,306 +67,288 @@ const columns = [
     },
 
     {
-    accessorKey: "download",
-    header: "➥",
-    size: 90, 
-    cell: (props) => {
-      return (
-      <button
-      // size="xs" 
-      // colorScheme="green"
-      // variant="solid"
-      // style={{
-      // width: "auto",     
-      // minWidth: "60px",  
-      // height: "20px",    
-      // padding: "0 4px",  
-      // fontSize: "0.7rem", 
-      // }}
-      // href={props.row.original.link_csv}
-      // download
-      // target="_blank"
-      // rel="noopener noreferrer"
-      onClick={() => {
-      //  const downloadLink = document.createElement("a");
-       downloadFile(props.row.original.link_csv);
-      //  downloadLink.href = "#";
-      //  downloadLink.download = `${props.row.original.link_csv}.csv`;
-      //  downloadLink.click();
-      }}
-      >
-      Baixar
-      </button>
-      );
-    },
+      accessorKey: "download",
+      header: "➥",
+      size: 90, 
+      cell: (props) => {
+        return (
+          <button
+            onClick={() => {
+              console.log(props.row.original.link_csv);
+              downloadFile(props.row.original.link_csv); // Chama a função de download com o link CSV
+            }}
+          >
+            Baixar
+          </button>
+        );
+      },
     },
 
+  {
+    //coluna "Nome_Municipio"
+    accessorKey: "Nome_Municipio",
+    header: "Nome Municipio",
+    size: 180,
+    cell: EditableCell,
+  },
+  {
+    //Coordenada_X
+    accessorKey: "Coordenada_X",
+    header: "Coord_X",
+    size: 120,
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+     //Coordenada_Y
+    accessorKey: "Coordenada_Y",
+    header: "Coord_Y",
+    size: 120,
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    //Ano_Inicio
+    accessorKey: "Ano_Inicio",
+    header: "Ano de Inicio",
+    size: 100,
+    cell: EditableCell,
+  },
+  {
+     //Ano_Fim
+    accessorKey: "Ano_Fim",
+    header: "Ano de Fim",
+    size: 100,
+    cell: EditableCell,
+  },
+  {
+     //Mes_Inicio
+    accessorKey: "Mes_Inicio",
+    header: "Mês de Inicio",
+    size: 100,
+    cell: EditableCell,
+  },
+  {
+    //Mes_Fim
+    accessorKey: "Mes_Fim",
+    header: "Mês de Fim",
+    size: 100,
+    cell: EditableCell,
+  },
+  //Dados diarios meteriolologicos
+   {
+    //Total_dias_intervalo
+    accessorKey: "Total_dias_intervalo",
+    header: "Nº total do intervalo",
+    size: 150,
+    cell: EditableCell,
+  },
+  {
+    //Dias_dados_medidos
+    accessorKey: "Dias_dados_medidos",
+    header: "Dias com dados medidos",
+    size: 160,
+    cell: EditableCell,
+  },
+  {
+    //Dias_falhos
+    accessorKey: "Dias_falhos",
+    header: "dias com falhas",
+    size: 130,
+    cell: EditableCell,
+  },
+  {
+    //Percentual_dias_falhos
+    accessorKey: "Percentual_dias_falhos",
+    header: "Percentual de dias com falhas (%)",
+    size: 190,
+    cell: EditableCell,
+  },
+  //Dados mensais meteriolologicos
+  {
+    //Total de meses do intervalo
+    accessorKey: "Total_meses_intervalo",
+    header: "Total de meses do intervalo",
+    size: 150,
+    cell: EditableCell,
+  },
+  {
+    ///Nº de meses completos
+    accessorKey: "Numero_meses_completos",
+    header: "Nº de meses completos",
+    size: 150,
+    cell: EditableCell,
+  },
+  {
+    //Nº de meses com falhas
+    accessorKey: "Numero_meses_falha",
+    header: "Nº de meses com falhas",
+    size: 150,
+    cell: EditableCell,
+  },
+  {
+    //Percentual de meses com falhas (%)
+    accessorKey: "Percentual_meses_falha",
+    header: "Percentual de meses com falhas (%)",
+    size: 190,
+    cell: EditableCell,
+  },
+//Dados Anuais meteriolologicos
+{
+  //Total de anos do intervalo
+  accessorKey: "Total_anos_intervalo",
+  header: "Total de anos do intervalo",
+  size: 150,
+  cell: EditableCell,
+},
+{
+  ///Nº de anos completos
+  accessorKey: "Numero_anos_completos",
+  header: "Nº de anos completos",
+  size: 150,
+  cell: EditableCell,
+},
+{
+  //Nº de anos com falhas
+  accessorKey: "Numero_anos_falha",
+  header: "Nº de anos com falhas",
+  size: 150,
+  cell: EditableCell,
+},
+{
+  //Percentual de anos com falhas (%)
+  accessorKey: "Percentual_anos_falha",
+  header: "Percentual de anos com falhas (%)",
+  size: 190,
+  cell: EditableCell,
+},
+//precipitação media anual
+{
+  //Precipitação média anual (mm)
+  accessorKey: "Precipitacao_media_anual",
+  header: "Precipitação média anual (mm)",
+  size: 175,
+  cell: EditableCell,
+},
 
-//     {
-//     //coluna "Nome_Municipio"
-//     accessorKey: "Nome_Municipio",
-//     header: "Nome Municipio",
-//     size: 180,
-//     cell: EditableCell,
-//   },
-//   {
-//     //Coordenada_X
-//     accessorKey: "Coordenada_X",
-//     header: "Coordenada X (m)",
-//     size: 180,
-//     cell: (props) => <p>{props.getValue()}</p>,
-//   },
-//   {
-//      //Coordenada_Y
-//     accessorKey: "Coordenada_Y",
-//     header: "Coordenada Y (m)",
-//     size: 180,
-//     cell: (props) => <p>{props.getValue()}</p>,
-//   },
-//   {
-//     //Ano_Inicio
-//     accessorKey: "Ano_Inicio",
-//     header: "Ano de Inicio",
-//     size: 160,
-//     cell: EditableCell,
-//   },
-//   {
-//      //Ano_Fim
-//     accessorKey: "Ano_Fim",
-//     header: "Ano de Fim",
-//     size: 160,
-//     cell: EditableCell,
-//   },
-//   {
-//      //Mes_Inicio
-//     accessorKey: "Mes_Inicio",
-//     header: "Mês de Inicio",
-//     size: 140,
-//     cell: EditableCell,
-//   },
-//   {
-//     //Mes_Fim
-//     accessorKey: "Mes_Fim",
-//     header: "Mês de Fim",
-//     size: 130,
-//     cell: EditableCell,
-//   },
-//   //Dados diarios meteriolologicos
-//    {
-//     //Total_dias_intervalo
-//     accessorKey: "Total_dias_intervalo",
-//     header: "Total de dias do intervalo",
-//     size: 225,
-//     cell: EditableCell,
-//   },
-//   {
-//     //Dias_dados_medidos
-//     accessorKey: "Dias_dados_medidos",
-//     header: "Número de dias com dados medidos",
-//     size: 225,
-//     cell: EditableCell,
-//   },
-//   {
-//     //Dias_falhos
-//     accessorKey: "Dias_falhos",
-//     header: "Número de dias com falhas",
-//     size: 225,
-//     cell: EditableCell,
-//   },
-//   {
-//     //Percentual_dias_falhos
-//     accessorKey: "Percentual_dias_falhos",
-//     header: "Percentual de dias com falhas (%)",
-//     size: 225,
-//     cell: EditableCell,
-//   },
-//   //Dados mensais meteriolologicos
-//   {
-//     //Total de meses do intervalo
-//     accessorKey: "Total_meses_intervalo",
-//     header: "Total de meses do intervalo",
-//     size: 225,
-//     cell: EditableCell,
-//   },
-//   {
-//     ///Nº de meses completos
-//     accessorKey: "Numero_meses_completos",
-//     header: "Nº de meses completos",
-//     size: 225,
-//     cell: EditableCell,
-//   },
-//   {
-//     //Nº de meses com falhas
-//     accessorKey: "Numero_meses_falha",
-//     header: "Nº de meses com falhas",
-//     size: 225,
-//     cell: EditableCell,
-//   },
-//   {
-//     //Percentual de meses com falhas (%)
-//     accessorKey: "Percentual_meses_falha",
-//     header: "Percentual de meses com falhas (%)",
-//     size: 225,
-//     cell: EditableCell,
-//   },
-// //Dados Anuais meteriolologicos
-// {
-//   //Total de anos do intervalo
-//   accessorKey: "Total_anos_intervalo",
-//   header: "Total de anos do intervalo",
-//   size: 225,
-//   cell: EditableCell,
-// },
-// {
-//   ///Nº de anos completos
-//   accessorKey: "Numero_anos_completos",
-//   header: "Nº de anos completos",
-//   size: 225,
-//   cell: EditableCell,
-// },
-// {
-//   //Nº de anos com falhas
-//   accessorKey: "Numero_anos_falha",
-//   header: "Nº de anos com falhas",
-//   size: 225,
-//   cell: EditableCell,
-// },
-// {
-//   //Percentual de anos com falhas (%)
-//   accessorKey: "Percentual_anos_falha",
-//   header: "Percentual de anos com falhas (%)",
-//   size: 225,
-//   cell: EditableCell,
-// },
-// //precipitação media anual
-// {
-//   //Precipitação média anual (mm)
-//   accessorKey: "Precipitacao_media_anual",
-//   header: "Precipitação média anual (mm)",
-//   size: 225,
-//   cell: EditableCell,
-// },
+{
+  //coluna "Precipitação em Janeiro"
+  accessorKey: "Mes_Jan",
+  header: "Jan (mm)",
+  size: 115,
+  cell: (props) => <p>{props.getValue()}</p>,
+  enableColumnFilter: true,
+  filterFn: "includesString",
 
-// {
-//   //coluna "Precipitação em Janeiro"
-//   accessorKey: "Mes_Jan",
-//   header: "Jan (mm)",
-//   size: 200,
-//   cell: (props) => <p>{props.getValue()}</p>,
-//   enableColumnFilter: true,
-//   filterFn: "includesString",
+},
+{
+  //coluna "Precipitação em Fervereiro"
+  accessorKey: "Mes_Fev",
+  header: "Feb (mm)",
+  size: 115,
+  cell: (props) => <p>{props.getValue()}</p>,
+  enableColumnFilter: true,
+  filterFn: "includesString",
 
-// },
-// {
-//   //coluna "Precipitação em Fervereiro"
-//   accessorKey: "Mes_Fev",
-//   header: "Feb (mm)",
-//   size: 200,
-//   cell: (props) => <p>{props.getValue()}</p>,
-//   enableColumnFilter: true,
-//   filterFn: "includesString",
+},
+{
+  //coluna "Precipitação em Fervereiro"
+  accessorKey: "Mes_Mar",
+  header: "Mar (mm)",
+  size: 115,
+  cell: (props) => <p>{props.getValue()}</p>,
+  enableColumnFilter: true,
+  filterFn: "includesString",
 
-// },
-// {
-//   //coluna "Precipitação em Fervereiro"
-//   accessorKey: "Mes_Mar",
-//   header: "Mar (mm)",
-//   size: 200,
-//   cell: (props) => <p>{props.getValue()}</p>,
-//   enableColumnFilter: true,
-//   filterFn: "includesString",
+},
+{
+  //coluna "Precipitação em Fervereiro"
+  accessorKey: "Mes_Apr",
+  header: "Apr (mm)",
+  size: 115,
+  cell: (props) => <p>{props.getValue()}</p>,
+  enableColumnFilter: true,
+  filterFn: "includesString",
 
-// },
-// {
-//   //coluna "Precipitação em Fervereiro"
-//   accessorKey: "Mes_Apr",
-//   header: "Apr (mm)",
-//   size: 200,
-//   cell: (props) => <p>{props.getValue()}</p>,
-//   enableColumnFilter: true,
-//   filterFn: "includesString",
+},
+{
+  //coluna "Precipitação em Fervereiro"
+  accessorKey: "Mes_May",
+  header: "May (mm)",
+  size: 115,
+  cell: (props) => <p>{props.getValue()}</p>,
+  enableColumnFilter: true,
+  filterFn: "includesString",
 
-// },
-// {
-//   //coluna "Precipitação em Fervereiro"
-//   accessorKey: "Mes_May",
-//   header: "May (mm)",
-//   size: 200,
-//   cell: (props) => <p>{props.getValue()}</p>,
-//   enableColumnFilter: true,
-//   filterFn: "includesString",
+},
+{
+  //coluna "Precipitação em Fervereiro"
+  accessorKey: "Mes_Jun",
+  header: "jun (mm)",
+  size: 115,
+  cell: (props) => <p>{props.getValue()}</p>,
+  enableColumnFilter: true,
+  filterFn: "includesString",
 
-// },
-// {
-//   //coluna "Precipitação em Fervereiro"
-//   accessorKey: "Mes_Jun",
-//   header: "jun (mm)",
-//   size: 200,
-//   cell: (props) => <p>{props.getValue()}</p>,
-//   enableColumnFilter: true,
-//   filterFn: "includesString",
+},
+{
+  //coluna "Precipitação em Fervereiro"
+  accessorKey: "Mes_Jul",
+  header: "Jul (mm)",
+  size: 115,
+  cell: (props) => <p>{props.getValue()}</p>,
+  enableColumnFilter: true,
+  filterFn: "includesString",
 
-// },
-// {
-//   //coluna "Precipitação em Fervereiro"
-//   accessorKey: "Mes_Jul",
-//   header: "Jul (mm)",
-//   size: 200,
-//   cell: (props) => <p>{props.getValue()}</p>,
-//   enableColumnFilter: true,
-//   filterFn: "includesString",
+},
+{
+  //coluna "Precipitação em Fervereiro"
+  accessorKey: "Mes_Aug",
+  header: "Aug (mm)",
+  size: 115,
+  cell: (props) => <p>{props.getValue()}</p>,
+  enableColumnFilter: true,
+  filterFn: "includesString",
 
-// },
-// {
-//   //coluna "Precipitação em Fervereiro"
-//   accessorKey: "Mes_Aug",
-//   header: "Aug (mm)",
-//   size: 200,
-//   cell: (props) => <p>{props.getValue()}</p>,
-//   enableColumnFilter: true,
-//   filterFn: "includesString",
+},
+{
+  //coluna "Precipitação em Fervereiro"
+  accessorKey: "Mes_Sep",
+  header: "Sep (mm)",
+  size: 115,
+  cell: (props) => <p>{props.getValue()}</p>,
+  enableColumnFilter: true,
+  filterFn: "includesString",
 
-// },
-// {
-//   //coluna "Precipitação em Fervereiro"
-//   accessorKey: "Mes_Sep",
-//   header: "Sep (mm)",
-//   size: 200,
-//   cell: (props) => <p>{props.getValue()}</p>,
-//   enableColumnFilter: true,
-//   filterFn: "includesString",
+},
+{
+  //coluna "Precipitação em Fervereiro"
+  accessorKey: "Mes_Oct",
+  header: "Oct (mm)",
+  size: 115,
+  cell: (props) => <p>{props.getValue()}</p>,
+  enableColumnFilter: true,
+  filterFn: "includesString",
 
-// },
-// {
-//   //coluna "Precipitação em Fervereiro"
-//   accessorKey: "Mes_Oct",
-//   header: "Oct (mm)",
-//   size: 200,
-//   cell: (props) => <p>{props.getValue()}</p>,
-//   enableColumnFilter: true,
-//   filterFn: "includesString",
+},
+{
+  //coluna "Precipitação em Fervereiro"
+  accessorKey: "Mes_Nov",
+  header: "Nov (mm)",
+  size: 115,
+  cell: (props) => <p>{props.getValue()}</p>,
+  enableColumnFilter: true,
+  filterFn: "includesString",
 
-// },
-// {
-//   //coluna "Precipitação em Fervereiro"
-//   accessorKey: "Mes_Nov",
-//   header: "Nov (mm)",
-//   size: 200,
-//   cell: (props) => <p>{props.getValue()}</p>,
-//   enableColumnFilter: true,
-//   filterFn: "includesString",
+},
+{
+  //coluna "Precipitação em Fervereiro"
+  accessorKey: "Mes_Dec",
+  header: "Dec (mm)",
+  size: 115,
+  cell: (props) => <p>{props.getValue()}</p>,
+  enableColumnFilter: true,
+  filterFn: "includesString",
 
-// },
-// {
-//   //coluna "Precipitação em Fervereiro"
-//   accessorKey: "Mes_Dec",
-//   header: "Dec (mm)",
-//   size: 200,
-//   cell: (props) => <p>{props.getValue()}</p>,
-//   enableColumnFilter: true,
-//   filterFn: "includesString",
-
-// },
+},
 
 ];
 
@@ -418,6 +407,7 @@ const TaskTable = () => {
         <Filters
           columnFilters={columnFilters}
           setColumnFilters={setColumnFilters}
+          
         />
         <Box className="table" w={table.getTotalSize()}>
           {table.getHeaderGroups().map((headerGroup) => (
@@ -540,6 +530,27 @@ const TaskTable = () => {
   >
     Baixar todos os postos
   </Button>
+   {/* <Button
+        onClick={() => {
+          alert(`Legendas:
+          TDD: Total de dias do intervalo
+          NDM: Número de dias com dados medidos
+          DF: Número de dias com falhas
+          PDF: Percentual de dias com falhas
+          TMI: Total de meses do intervalo
+          NMC: Número de meses completos
+          NMCF: Número de meses com falhas
+          PMF: Percentual de meses com falhas
+          TAI: Total de anos do intervalo
+          NAC: Número de anos completos
+          NAF: Número de anos com falhas
+          PAF: Percentual de anos com falhas
+          PMA: Precipitação média anual`);
+        }}
+      >
+        ℹ️
+      </Button> */}
+      <br/>
     </Box>
 
         </Box>    
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/pluviometria/",
   server: {
     watch: {
       usePolling: true,
+    },
+    cors: false, 
+  },
+  resolve: {
+    alias: {
+      styles: './src/theme/styles.js',
     },
   },
 });
