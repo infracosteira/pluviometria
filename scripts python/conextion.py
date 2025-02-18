ddecf0b314267164f1af0d0c6fdaa010befd4ef8
YURISAMY <isaak.yuri@aluno.unilab.edu.br>
2025-02-18 15:15:18 -0300

    script desnecessario.

--- scripts python/conextion.py
+++
@@ -1,8 +0,0 @@
-import httpx
-
-url = "https://hqnkhorlbswlklcfvoob.supabase.co"
-try:
-    response = httpx.get(url, timeout=10)
-    print(response.status_code)
-except httpx.HTTPError as e:
-    print(f"Erro: {e}")
