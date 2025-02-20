b792d964ee5b74f0a4b0104345bb2b85cf53919a
YURISAMY <isaak.yuri@aluno.unilab.edu.br>
2025-02-20 17:06:40 -0300

    Finalização do script de insersão dos dados.

--- scripts python/db_insert_data.py
+++ scripts python/db_insert_data.py
@@ -1,37 +1,26 @@
-import pandas as pd
-from supabase import create_client, Client
 from dotenv import load_dotenv
-import os
-
-# Carregar variáveis de ambiente
 load_dotenv()
+import os
+from supabase import create_client, Client
+import pandas as pd
 
-supabase_url = os.getenv("SUPABASE_URL")
-supabase_key = os.getenv("SUPABASE_KEY")
+supabase_url = os.environ.get("SUPABASE_URL")
+supabase_key = os.environ.get("SUPABASE_KEY")
 
-print("Chave e URL copiadas com sucesso!")
+print("Chave e url copiados com sucesso!")
+print(f"URL: {supabase_url}, KEY: {supabase_key[:5]}...") 
 
-# Criar cliente Supabase
-supabase: Client = create_client(supabase_url, supabase_key)
+supabase = create_client(supabase_url, supabase_key)
 
-def upload_dataframe_to_supabase(dataframe: pd.DataFrame, table_name: str):
-    
-    data = dataframe.to_dict(orient="records")
+print("Cliente Supabase criado com sucesso!")
 
-    try:
-        response = supabase.table(table_name).insert(data).execute()
-        print(f"Resposta do Supabase: {response}")
-        
-        if "error" not in response:
-            print(f"✅ Dados importados com sucesso para a tabela '{table_name}'")
-        else:
-            print(f"❌ Erro ao importar dados: {response['error']}")
-    except Exception as e:
-        print(f"⚠ Ocorreu um erro ao inserir os dados: {e}")
-
-# Ler CSV
 df = pd.read_csv('data/maindatabase.csv')
 table_name = "pluviometria"
 
-# Importar o DataFrame
-upload_dataframe_to_supabase(df, table_name)
+test_data = df.to_dict(orient='records')
+
+batch_size = 1000  
+for i in range(0, len(test_data), batch_size):
+    batch = test_data[i:i + batch_size]
+    response = supabase.table("pluviometria").insert(batch).execute()
+    print(f"Lote {i // batch_size + 1} inserido com sucesso! Resposta:", response)
