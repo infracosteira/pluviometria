cfa60f94ac582dd337f56d0f8b4a017c6f73344e
Isaak Yuri <isaak.yuri@aluno.unilab.edu.br>
2025-04-15 16:03:47 -0300

    df att

--- fast_one/fast_one/df_import.py
+++ fast_one/fast_one/df_import.py
@@ -1,27 +1,19 @@
 import os
-
 import pandas as pd
 from dotenv import load_dotenv
-from supabase import Client, create_client
+from supabase import create_client, Client
 
 load_dotenv()
-url = os.environ['SUPABASE_URL']
-key = os.environ['SUPABASE_KEY']
+url = os.environ["SUPABASE_URL"]
+key = os.environ["SUPABASE_KEY"]
 supabase: Client = create_client(url, key)
 
-
 def load_municipio():
-    return pd.DataFrame(supabase.table('municipio').select('*').execute().data)
-
+    return pd.DataFrame(supabase.table("municipio").select("*").execute().data)
 
 def load_posto():
-    return pd.DataFrame(supabase.table('posto').select('*').execute().data)
-
+    return pd.DataFrame(supabase.table("posto").select("*").execute().data)
 
 def load_registro():
-    return pd.DataFrame(supabase.table('registro').select('*').execute().data)
+    return pd.DataFrame(supabase.table("registro").select("*").execute().data)
 
-
-municipio = load_municipio()
-
-print(municipio.head(5))
