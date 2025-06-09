7845b22e84384c1429b070c708b822b549445c88
Isaak Yuri <isaak.yuri@aluno.unilab.edu.br>
2025-06-09 19:01:37 -0300

    models att

--- fast_one/fast_one/models.py
+++ fast_one/fast_one/models.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import ForeignKey, text
+from sqlalchemy import ForeignKey,Date, text
 from db_conection import reg, engine
+from geoalchemy2 import Geometry
+from typing import Any
+from datetime import datetime
 
 
 @reg.mapped_as_dataclass
@@ -13,10 +16,10 @@ class NomeMunicipio:
 
 @reg.mapped_as_dataclass
 class Registro:
-    __tablename__ = "registro"
+    __tablename__ = "registro_mensal"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    total_dia: Mapped[float] = mapped_column()
+    total_mes: Mapped[float] = mapped_column()
     mes: Mapped[int] = mapped_column()
     ano: Mapped[int] = mapped_column()
     id_posto: Mapped[int] = mapped_column(ForeignKey("posto.id_posto"))
@@ -34,7 +37,16 @@ class NomePosto:
     numero_anos_falha: Mapped[int] = mapped_column()
     numero_anos_medidos: Mapped[int] = mapped_column()
     precipitacao_media_anual: Mapped[float] = mapped_column()
-    coordenadas: Mapped[str] = mapped_column() 
+    coordenadas: Mapped[any] = mapped_column(Geometry(geometry_type="POINT", srid=4326))
+
+@reg.mapped_as_dataclass
+class NomeEstado:
+    __tablename__ = "registro-diario"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    id_posto: Mapped[int] = mapped_column(ForeignKey("posto.id_posto"))
+    data: Mapped[datetime] = mapped_column(Date)
+    valor: Mapped[float] = mapped_column()
 
 reg.metadata.create_all(engine)
 
